ffc32587a9999155a3b95029a27c5ba317efdaa6
malindu ruwantha <malinduruwantha@gmail.com>
2018-03-28 19:56:38 +0530

    Initial commit

---
+++ hire.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<title></title>
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+  <link rel="stylesheet" type="text/css" href="css/fa/css/font-awesome.min.css">
+  <link href="https://fonts.googleapis.com/css?family=Lobster" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css?family=Bree+Serif" rel="stylesheet">
+  <link rel="stylesheet" type="text/css" href="css/style.css">
+  <script src="js/jquery-3.2.1.min.js"></script>
+  <script src="js/propper.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+
+</head>
+<body>
+<div class="container-fluid">
+	<!-- navigation bar -->
+    <div>
+      <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+          <div class="container">
+            <a class="navbar-brand" href="#">MAKEYOURRIDE</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarResponsive">
+              <ul class="navbar-nav ml-auto">
+                <li class="nav-item active">
+                  <a class="nav-link f2" href="#">Home
+                    <span class="sr-only f2">(current)</span>
+                  </a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link f2" href="#">About</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link f2" href="#">Services</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#"></a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link f2" href="#">sign in</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link f2" href="#">join with us</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </nav>
+
+       
+      </div>   
+
+
+<!-- user types -->
+<div class="row row_register">
+	
+	<div class="col-md-4">
+		<h2>Register Your boat</h2>
+
+		<img src="images/ddd.jpg" class="img2" alt="">
+		<p>start your carrier as a boat owner.join with us </p>
+		
+	
+		<button type="button" class="btn btn-success">REGISTER</button>
+
+	</div>
+
+	<div class="col-md-4">
+		<h2>Register as a Photographer</h2>
+		
+		<img src="images/sss.jpg" alt="" class="img2">
+		<p>Are you a good photographer we will make you some money..earn here by registering your self</p>
+		
+		<button type="button" class="btn btn-success">REGISTER</button>
+	</div>
+
+	<div class="col-md-4">
+		<h2>Register your Residance</h2>
+		
+		<img src="images/ssss.jpg" alt="" class="img2">
+		<p>register your hotel here and gain daily tourist visits</p>
+		
+		<button type="button" class="btn btn-success">REGISTER</button>
+	</div>
+</div>
+
+</div>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,366 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+   <link rel="stylesheet" type="text/css" href="css/fa/css/font-awesome.min.css">
+   <link href="https://fonts.googleapis.com/css?family=Lobster" rel="stylesheet">
+   <link href="https://fonts.googleapis.com/css?family=Bree+Serif" rel="stylesheet">
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+  <script src="js/jquery-3.2.1.min.js"></script>
+  <script src="js/propper.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+
+</head>
+
+<body>
+ 
+<div class="container-fluid">
+ 
+<!-- navigation bar -->
+<div>
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+      <div class="container">
+        <a class="navbar-brand" href="#">MAKEYOURRIDE</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+          <ul class="navbar-nav ml-auto">
+            <li class="nav-item active">
+              <a class="nav-link f2" href="#">Home
+                <span class="sr-only f2">(current)</span>
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link f2" href="#">About</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link f2" href="#">Services</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#"></a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link f2" href="#">sign in</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link f2" href="#">reserve your time</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+
+   
+  </div>   
+  
+  <!-- slider -->
+        <div id="carouselExampleIndicators" class="carousel slide carousel-fade" data-ride="carousel" data-interval="4000" >
+          <ol class="carousel-indicators">
+            <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+            <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+            <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+          </ol>
+          <div class="carousel-inner " role="listbox">
+            <div class="carousel-item active ">
+              <img class="d-block img-fluid customslide imgc" src="images/q.jpg" alt="First slide">
+              <div class="carousel-caption d-none d-md-block" style="margin-top: -300px">
+                         <h3 class="f2">jsfchjsafjhsajhfdshdfjhsad</h3>
+                          <p class="f2">kjsahdkjsakjdg</p>
+                            <button type="" class="btn btn-success btn-lg">GET STARTED</button>
+               </div>
+               <div class="carousel-caption d-none d-md-block" style="margin-top: -300px">
+                        
+               </div>
+            </div>
+            <div class="carousel-item  ">
+              <img class="d-block img-fluid customslide imgc" src="images/x.jpg" alt="Second slide">
+              <div class="carousel-caption d-none d-md-block" style="margin-top: -300px">
+                         <h3 class="f2">jsfchjsafjhsajhfdshdfjhsad</h3>
+                          <p class="f2">kjsahdkjsakjdg</p>
+                            <button type="" class="btn btn-success btn-lg">GET STARTED</button>
+               </div>
+               <div class="carousel-caption d-none d-md-block" style="margin-top: -300px">
+                         
+               </div>
+            </div>
+            <div class="carousel-item ">
+              <img class="d-block img-fluid customslide imgc" src="images/s.jpg" alt="Third slide">
+               <div class="carousel-caption d-none d-md-block" style="margin-top: -300px">
+                         <h3 class="f2">jsfchjsafjhsajhfdshdfjhsad</h3>
+                          <p class="f2">kjsahdkjsakjdg</p>
+                            <button type="" class="btn btn-success btn-lg ">GET STARTED</button>
+               </div>
+            </div>
+          </div>
+          <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+          </a>
+          <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+          </a>
+
+</div>
+            
+<!-- about section --> 
+  <div class="backgroundabout  ">
+    <div class="row"">
+      <div class="col-md-6 abouttext">
+        <h1 class="f2 aboutust">About US</h1>
+        <span class="fa fa-lightbulb-o fa-5x iconbulb" aria-hidden="true" ></span> 
+      </div>
+      <div class="col-md-6 abouttext">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+        consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+        cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+        proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+        consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+        cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+        proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+      </div>
+    </div>
+  </div>       
+       
+ <!-- events section --> 
+ <div class="blockevents">
+   
+
+ <div class="row">
+     <div class="col-md-12"> 
+       <h1 class="headertext f2 centert">Join With Us</h1>   
+      </div>   
+</div>     
+<div class="row servicesrow">
+  <div class="col-md-3 servicescontent">
+    <img src="images/d.jpg " alt="" class="img2">
+    <h3  class="f1 centert">Corel Reefs</h3>
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+    consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+    cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+    proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+    <br>
+    <button type="" class="btn btn-danger">BOOK NOW</button>
+    <button type="" class="btn btn-outline-primary">Read More</button>
+  </div>
+
+  <div class="col-md-3 servicescontent">
+    <img src="images/y.jpg" alt="" class="img2" >
+    <h3  class="f1 centert">Arrange Your Dinner</h3>
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+    consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+    cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+    proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+    <br>
+    <button type="" class="btn btn-danger">BOOK NOW</button>
+    <button type="" class="btn btn-outline-primary">Read More</button>
+  </div>
+
+  <div class="col-md-3 servicescontent">
+    <img src="images/f.jpg" alt="" class="img2">
+    <h3  class="f1 centert">Serfing</h3>
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+    consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+    cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+    proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+    <br>
+    <button type="" class="btn btn-danger">BOOK NOW</button>
+    <button type="" class="btn btn-outline-primary">Read More</button>
+  </div>
+
+  <div class="col-md-3 servicescontent">
+    <img src="images/v.jpg" alt="" class="img2">
+    <h2 class="f1 centert">Whale watching</h2>
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+    consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+    cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+    proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+    <br>
+    <button type="" class="btn btn-danger">BOOK NOW</button>
+    <button type="" class="btn btn-outline-primary">Read More</button>
+  </div>
+
+</div>
+ </div>
+
+
+<!-- other services -->
+
+  <div class="servicesback">
+    <div class="row">
+      <div class="col-md-12">
+        <h1 class="f2 whitecol headertext centert">Other Services</h1> 
+      </div>
+    </div>
+    <div class="row">
+
+      <div class="col-md-4">
+         <span class="fa fa-camera fa-3x iconbulb1 centert" aria-hidden="true" ></span>
+         <br>
+         <br>
+         <h1 class="f2 centert whitecol">PHOTOGRAPHY</h1> 
+      </div>
+
+      <div class="col-md-4">
+         <span class="fa fa-car fa-5x iconbulb1 centert" aria-hidden="true" ></span>
+            <br>
+          <br>
+         <h1 class="f2 centert whitecol">TRANSPORT</h1> 
+      
+      </div>
+
+      <div class="col-md-4">
+        <span class="fa fa-home fa-5x iconbulb1 centert" aria-hidden="true" ></span> 
+        <br>
+         <br>
+        <h1 class="f2 centert whitecol">BOOK YOUR HOTEL</h1>
+
+      </div>
+
+    </div>
+  </div >
+<!-- gallery section -->
+<div class="gallerylock">
+  
+
+<div class="row ">
+  <div class="col-md-3  galleryrow">
+    <img src="images/f.jpg " alt="" class="img2">
+  </div>
+  <div class="col-md-3  galleryrow">
+    <img src="images/q.jpg " alt="" class="img2">
+  </div>
+  <div class="col-md-3  galleryrow">
+    <img src="images/v.jpg " alt="" class="img2">
+  </div>
+  <div class="col-md-3  galleryrow">
+  <img src="images/x.jpg " alt="" class="img2">
+  </div>
+  
+</div>
+<div class="row ">
+  <div class="col-md-3  galleryrow">
+    <img src="images/whale.jpg " alt="" class="img2" style="width:100%">
+  </div>
+  <div class="col-md-3  galleryrow">
+     <img src="images/d.jpg " alt="" class="img2">
+  </div>
+  <div class="col-md-3  galleryrow">
+    <img src="images/x.jpg " alt="" class="img2">
+  </div>
+  <div class="col-md-3  galleryrow">
+    <img src="images/y.jpg " alt="" class="img2">
+  </div>
+</div>
+</div>
+
+ 
+<!-- contact form -->
+<div class="row">
+  
+</div>
+<!-- mobile -->
+<hr>
+
+<div class="col-md-12 mobileback"  >
+  <div class="row">
+    <div class="col-md-8">
+      <br>
+        <h2 class="f1 " >Download your mobile application Now</h2>
+       
+      <h2 class="f2 ">Quick access through our mobile application</h2>
+      <br>
+      <h5 class="f2 ">this will make up your boat rides <br>and your sea shore travels at your fingertips</h5>
+      <br><br>
+           <!-- apple store button -->
+                <a class="btn btn-lg btn-primary big-btn" href="#">
+          <i class="glyphicon glyphicon-phone pull-left"></i><div class="btn-text"><small>Available on the</small><br><strong>App Store</strong></div></a>
+            <!-- android button -->
+          <a class="btn btn-lg btn-success big-btn android-btn" href="#">
+          <img width="80px" class="pull-left" src="http://www.userlogos.org/files/logos/jumpordie/google_play_04.png"><div class="btn-text"><small>Available on</small><br><strong>Google Play</strong></div></a>
+            <!-- windows store button -->
+                <a class="btn btn-lg btn-success big-btn windows-btn" href="#">
+          <img width="60px" class="pull-left" src="http://1.bp.blogspot.com/-rD2_M6Vvv6w/UbmywLq5V8I/AAAAAAAACko/BEcR4QqtkRI/s1600/Windows%2BStore%2BLogo.png"><div class="btn-text">Windows<br>Store</div></a>
+                   
+            
+        </div>
+
+        <div class="col-md-4">
+          <img src="images/m.png" alt="" class="" style="width:70% ;height:500px">
+        </div>
+    </div>
+</div>
+<hr>
+<!-- footer -->
+  
+ <div class="footerblock">
+       <footer>
+     <div class="container">
+       <div class="row">
+       
+                <div class="col-md-4 col-sm-6 col-xs-12">
+                
+                </div>
+                
+                <div class="col-md-4 col-sm-6 col-xs-12">
+                    <ul class="menu">
+                         <span>Menu</span>    
+                         <li>
+                            <a href="#">Home</a>
+                          </li>
+                               
+                          <li>
+                             <a href="#">About</a>
+                          </li>
+                               
+                          <li>
+                            <a href="#">Blog</a>
+                          </li>
+                               
+                          <li>
+                             <a href="#">Gallery </a>
+                          </li>
+                     </ul>
+                </div>
+           
+                <div class="col-md-4 col-sm-6 col-xs-12">
+                  <ul class="address">
+                        <span>Contact</span>       
+                        <li>
+                           <i class="fa fa-phone" aria-hidden="true"></i> <a href="#">Phone</a>
+                        </li>
+                        <li>
+                           <i class="fa fa-map-marker" aria-hidden="true"></i> <a href="#">Adress</a>
+                        </li> 
+                        <li>
+                           <i class="fa fa-envelope" aria-hidden="true"></i> <a href="#">Email</a>
+                        </li> 
+                   </ul>
+               </div>
+           
+           
+           </div> 
+
+    </footer>
+
+ </div> 
+
+
+</div>
+
+</body>
+</html>
