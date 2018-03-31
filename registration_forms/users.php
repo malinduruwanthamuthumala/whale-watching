fddb74f60968209cfc974101bff030d569d49587
malindu ruwantha muthumala <32791260+malinduruwanthamuthumala@users.noreply.github.com>
2018-03-31 12:05:31 +0530

    added registraton folder and a user registration form

---
+++ registration_forms/users.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<link rel="stylesheet" type="text/css" href="../css/bootstrap.min.css">
+  <link rel="stylesheet" type="text/css" href="../css/fa/css/font-awesome.min.css">
+  <link href="https://fonts.googleapis.com/css?family=Lobster" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css?family=Bree+Serif" rel="stylesheet">
+	<link rel="stylesheet" type="text/css" href="../css/style.css">
+  <script src="../js/jquery-3.2.1.min.js"></script>
+  <script src="../js/propper.js"></script>
+  <script src="../js/bootstrap.min.js"></script>
+</head>
+<body>
+
+<div class="container-fluid">
+	
+<div class="row">
+	
+
+	<div class="col-md-4">
+		
+	</div>
+
+	<div class="col-md-4">
+		<form action="users_submit" method="post" accept-charset="utf-8" target="" class="form-group">
+			<label>First Name</label>
+			<input type="" name="" class="form-control">
+
+			<label>Last Name</label>
+			<input type="" name=""  class="form-control">
+
+			<label>Email</label>
+			<input type="" name=""  class="form-control">
+
+			<label>Password</label>
+			<input type="" name=""  class="form-control">
+
+			<label>telephone</label>
+			<input type="" name=""  class="form-control">
+
+			<label>Usertype</label>
+			<select name="usertype"   class="form-control">
+				<option value=""  class="form-control">admin</option>
+				<option value=""  class="form-control">accountant</option>
+				<option value=""  class="form-control">developer</option>
+				<option value=""  class="form-control">Manager</option>
+			
+				
+			</select>
+			<br>
+			<button type="submit" class="btn btn-outline-info">SUBMIT</button>
+			<button type="Reset" class="btn btn-outline-danger">RESET</button>
+		</form>
+	</div>
+
+	<div class="col-md-4">
+		
+	</div>
+
+</div>
+</div>
+
+
+
+
+</body>
+</html>
